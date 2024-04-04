9c824d21bef9926d7eaca7db292a558d6012d5a3
Paweł Kruszec <pakruszec@gmail.com>
2024-04-04 13:23:38 +0200

    pod - working on tests, but not on szkopul

--- pod/pod.cpp
+++ pod/pod.cpp
@@ -1,24 +1,75 @@
 #include <iostream>
 #include <fstream>
 
-int main(void) {
+// test1.txt -- 11
+// test2.txt -- 8
+// test3.txt -- 15
+//
+// Podejścia:
+// 1. Podejście testowe (wynik: 0)
+//
+
+int main(int num_args, char **args) {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
     std::cout.tie(0);
 
-#if POD_DEBUG
-    std::ifstream in("test1.txt");
+#ifdef POD_DEBUG
+    char const *filename = 0;
+    if (num_args < 2) {
+        filename = "test1.txt";
+    } else {
+        filename = args[1];
+    }
+
+    std::ifstream in(filename);
     std::cin.rdbuf(in.rdbuf());
 #endif
 
     int count = 0;
-    int num_divs = 0;
-    std::cin >> count >> num_divs;
+    int num_parts = 0;
+    std::cin >> count >> num_parts;
+
+    int *data = new int[count];
 
     for (int i = 0; i < count; ++i) {
         int current;
         std::cin >> current;
+        data[i] = current;
     }
     
+    int num_divs = num_parts - 1;
+
+    int s = 1;
+    while (true) {
+        int sum = 0;
+        int divs_made = 0;
+        bool valid = true;
+        for (int i = 0; i < count; ++i) {
+            sum += data[i];
+            if (sum > s) {
+                divs_made++;
+                sum = data[i];
+
+                if (sum > s) {
+                    divs_made++;
+                }
+            }
+            if (divs_made > num_divs) {
+                valid = false;
+                break;
+            }
+        }
+
+        if (valid) {
+            break;
+        } else {
+            s++;
+        }
+
+    }
+
+    std::cout << s;
+
     return 0;
 }
