1582b544a02a06b20cb9530988388963c55447f6
Paweł Kruszec <pakruszec@gmail.com>
2024-10-15 14:47:29 +0200

    move wal tests

--- wal/build.bat
+++ wal/build.bat
@@ -3,15 +3,7 @@
 clang++ wal.cpp -std=c++20 -o wal.exe
 
 echo === MANUAL TEST OUTPUT ===
-call wal < input01.txt
-call wal < input02.txt
-call wal < input03.txt
-call wal < input04.txt
-call wal < input05.txt
-call wal < input06.txt
-call wal < input07.txt
-call wal < input08.txt
-call wal < input09.txt
+for /f %%f in ('dir /b input') do call wal < input\%%f
 
 echo === GENERATED TEST OUTPUT ===
 for /f %%f in ('dir /b test') do call wal < test\%%f
