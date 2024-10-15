5dcb758dbb8133d5a360275cc306955a6942dc7c
Paweł Kruszec <pakruszec@gmail.com>
2024-10-15 15:49:46 +0200

    wal

--- wal/wal.cpp
+++ wal/wal.cpp
@@ -63,49 +63,5 @@ int main()
         cout << "NIE\n";
     }
 
-    // cout << "FIRST : " << fst.first << "," << fst.second << "\n";
-    // cout << "SECOND: " << snd.first << "," << snd.second << "\n";
-
-    // bool fst_gone = (fst.first < snd.first) || (fst.second < snd.second);
-    // bool snd_gone = (snd.first < fst.first) || (snd.second < fst.second);
-
-    // cerr << "FIRST: " << fst.first << " " << fst.second << " gone? " << fst_gone << "\n";
-    // cerr << "SECND: " << snd.first << " " << snd.second << " gone? " << snd_gone << "\n";
-
-    // if (fst_gone && snd_gone) {
-    //     cout << "TAK\n";
-    // } else {
-    //     cout << "NIE\n";
-    // }
-
-/*
-    vector<pair<int, int>> xs(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> xs[i].first >> xs[i].second;
-    }
-    while (!xs.empty()) {
-        if (xs.size() == 1) {
-            cout << "NIE\n";
-            return 0;
-        }
-
-        auto [s0, z0] = xs[0];
-        auto [s1, z1] = xs[1];
-
-        bool remove0 = (s0 < s1) || (z0 < z1);
-        bool remove1 = (s1 < s0) || (z1 < z0);
-        if (remove0) {
-            xs.erase(xs.begin());
-            if (remove1) {
-                xs.erase(xs.begin());
-            }
-        } else if (remove1) {
-            xs.erase(xs.begin() + 1);
-        }
-    }
-
-    cout << "TAK\n";
-*/
-
     return 0;
 }
