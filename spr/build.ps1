52333f44fb52b133e59487459530b1ee07845cb6
Paweł Kruszec <pakruszec@gmail.com>
2024-10-29 21:51:34 +0100

    spr mixed

--- spr/build.ps1
+++ spr/build.ps1
@@ -12,23 +12,26 @@ function Run-Test-Folder {
 }
 
 function Run-BigTest {
-    param($in, $out)
+    param($in)
     foreach ($file in Get-ChildItem($in)) {
-        $name = [System.IO.Path]::GetFileName($file)
-        $new =  [System.IO.Path]::ChangeExtension($name, '.out')
-        $path = [System.IO.Path]::Combine($out, $new)
 
-        $expected = Get-Content $path
+        $name = [System.IO.Path]::GetFileName($file)
+        # $new =  [System.IO.Path]::ChangeExtension($name, '.out')
+        # $path = [System.IO.Path]::Combine($out, $new)
+        # $expected = Get-Content $path
+
         $output = Get-Content -Raw $file.FullName | .\spr.exe
 
-        Write-Host $new '---' $time 's' 'got' $output
-        if ($expected -ne $output) {
+        if ($output -eq "BUG baj jealous" -or $output -eq "BUG bit jealous" -or $output -eq "BUG min_index < 0") {
+            Write-Host $name '---' 'got' $output
         }
     }
 }
 
 clang++ spr.cpp -std=c++20 -o spr.exe
-Run-BigTest "in" "out"
 
-# Run-Test "in/spr3ocen.in"
+Run-BigTest "in"
+Run-BigTest "C:\home\tmp\spr\testy"
+
+# Run-Test "C:\home\tmp\spr\testy\spr1.in"
 # Run-Test "in/spr2ocen.in"
--- spr/spr.cpp
+++ spr/spr.cpp
@@ -2,12 +2,19 @@
 #include <vector>
 #include <stdint.h>
 #include <limits.h>
+#include <algorithm>
 
 using namespace std;
 
 using s64 = int64_t;
 
 int n = 0;
+s64 full_sum_a = 0;
+s64 full_sum_i = 0;
+s64 sum_a = 0;
+s64 sum_i = 0;
+s64 min_a = INT_MAX;
+vector<int> xs;
 vector<int> ba;
 vector<int> bi;
 vector<bool> owner;
@@ -25,9 +32,9 @@ bool jealous(bool who)
             sum_other += source[i];
             if (source[i] < min_other) min_other = source[i];
         }
-        // cout << who << ", " << i << ": " << sum_this << " vs " << sum_other << "\n";
     }
 
+    // cout << who << ": " << sum_this << " vs " << sum_other << "\n";
     return sum_this < (sum_other - min_other);
 }
 
@@ -38,35 +45,86 @@ int main()
     cout.tie(0);
 
     cin >> n;
+
+    if (n == 1) {
+        cout << "0\n";
+        return 0;
+    }
+    if (n == 2) {
+        cout << "0 1\n";
+        return 0;
+    }
+
+    xs.resize(n);
     ba.resize(n);
     bi.resize(n);
     owner.resize(n, false);
 
+    for (int i = 0; i < n; ++i) {
+        xs[i] = i;
+    }
+
     for (auto &x: ba) {
         cin >> x;
+        full_sum_a += x;
+        if (x < min_a) min_a = x;
     }
     for (auto &x: bi) {
         cin >> x;
+        full_sum_i += x;
     }
 
-    while (jealous(1)) {
+    sum_a = full_sum_i;
+
+    sort(xs.begin(), xs.end(), [](auto a, auto b){
+            // return bi[a] > bi[b];
+            return bi[a] > bi[b] + ba[a] < ba[b];
+        });
+
+    // for (auto x: xs) {
+    //     cout << x << " ";
+    // }cout << "\n";
+
+    // cout << sum_i << " vs " << sum_a << "-" << min_a << "=" << (sum_a-min_a) << "\n";
+
+    int p = 0;
+    while (true) {
         int min = INT_MAX;
         int min_index = -1;
+
+        for (int i = p; i < xs.size(); ++i) {
+            int x = xs[i];
+            if (!owner[x]) {
+                min_index = x;
+                min = bi[x];
+                break;
+            }
+        }
+
+        /*
         for (int i = 0; i < n; ++i) {
             if (!owner[i] && ba[i] < min) {
                 min = ba[i];
                 min_index = i;
             }
         }
+        */
 
         if (min_index < 0) {
             cout << "BUG min_index < 0\n";
             return 0;
         }
 
+        min_a = min;
+        // cout << min << ": " << sum_i << " vs " << sum_a << "-" << min_a << "=" << (sum_a-min_a) << "\n";
+        if (sum_i >= (sum_a-min_a)) break;
+
         // cout << min_index << "\n";
 
+        sum_a -= bi[min_index];
+        sum_i += bi[min_index];
         owner[min_index] = true;
+        p = min_index;
 
         // for (auto x: owner) {
         //     cout << x << " ";
@@ -74,10 +132,16 @@ int main()
         // cout << "\n";
     }
     
+#if 1
     if (jealous(0)) {
-        cout << "BUG jealous\n";
-        return 0;
+        cout << "BUG baj jealous\n";
+        // return 0;
     }
+    if (jealous(1)) {
+        cout << "BUG bit jealous\n";
+        // return 0;
+    }
+#endif
 
     for (auto x: owner) {
         cout << x << " ";
