de2a83e5fb487f2b7f32b530da35474e9e50c875
Paweł Kruszec <pakruszec@gmail.com>
2025-01-29 12:01:33 +0100

    tro 100 FL

--- szkopul/tro/tro.cpp
+++ szkopul/tro/tro.cpp
@@ -5,24 +5,30 @@
 #include <queue>
 #include <unordered_map>
 #include <unordered_set>
+#include <stdint.h>
 
 using namespace std;
 
-// struct H {
-//     size_t operator()(const tuple<int, int, int> &x) const
-//     {
-//         return hash<int>()(get<0>(x)) ^ hash<int>()(get<1>(x)) ^ hash<int>()(get<2>(x));
-//         // return hash<decltype(get<0>(x))>()(get<0>(x));
-//     }
-// };
+/*
 
-unordered_map<int, vector<int>> e;
-vector<tuple<int, int, int>> tri;
+3 -> 1
+4 -> 4
 
-template<typename U, typename T>
-bool has(U &xs, T x)
+n!/k!(n-k)!
+
+*/
+
+vector<uint64_t> e;
+
+uint64_t fact(uint64_t a)
 {
-    return find(xs.begin(), xs.end(), x) != xs.end();
+    if (a == 0) return 1;
+
+    uint64_t x = 1;
+    for (uint64_t i = 1; i <= a; ++i) {
+        x *= i;
+    }
+    return x;
 }
 
 int main()
@@ -30,86 +36,27 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int n, m;
+    uint64_t n, m;
     cin >> n >> m;
 
-    for (int i = 0; i < m; ++i) {
-        int p, k;
+    e.resize(n+1);
+
+    for (uint64_t i = 0; i < m; ++i) {
+        uint64_t p, k;
         cin >> p >> k;
-        
-        e[p].push_back(k);
-        e[k].push_back(p);
+
+        e[p]++;
+        e[k]++;
     }
 
-    // # one-color triangles = (# of triples of points that are all connected) + (# of triples of points that are all not connected)
+    uint64_t j = n*(n-1)*(n-2) / 6;
 
-    int red = 0;
-
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (i == j) continue;
-            if (!has(e[i], j)) continue;
-
-            for (int k = 1; k <= n; ++k) {
-                if (i == k) continue;
-                if (j == k) continue;
-
-                if (!has(e[i], k)) continue;
-                if (!has(e[j], k)) continue;
-
-                if (has(tri, tuple<int, int, int>{i, j, k})) continue;
-                // if (has(tri, tuple<int, int, int>{i, k, j})) continue;
-                // if (has(tri, tuple<int, int, int>{j, k, i})) continue;
-                // if (has(tri, tuple<int, int, int>{j, i, k})) continue;
-                // if (has(tri, tuple<int, int, int>{k, j, i})) continue;
-                // if (has(tri, tuple<int, int, int>{k, i, j})) continue;
-
-                red++;
-                //tri.push_back({i, j, k});
-                tri.push_back({i, j, k});
-                tri.push_back({i, k, j});
-                tri.push_back({j, k, i});
-                tri.push_back({j, i, k});
-                tri.push_back({k, j, i});
-                tri.push_back({k, i, j});
-            }
-        }
+    uint64_t s = 0;
+    for (uint64_t i = 1; i <= n; ++i) {
+        s += (e[i] * (n-1-e[i])) / 2;
     }
 
-    int black = 0;
+    cout << j-s << "\n";
 
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (i == j) continue;
-            if (has(e[i], j)) continue;
-
-            for (int k = 1; k <= n; ++k) {
-                if (i == k) continue;
-                if (j == k) continue;
-
-                if (has(e[i], k)) continue;
-                if (has(e[j], k)) continue;
-
-                if (has(tri, tuple<int, int, int>{i, j, k})) continue;
-                // if (has(tri, tuple<int, int, int>{i, k, j})) continue;
-                // if (has(tri, tuple<int, int, int>{j, k, i})) continue;
-                // if (has(tri, tuple<int, int, int>{j, i, k})) continue;
-                // if (has(tri, tuple<int, int, int>{k, j, i})) continue;
-                // if (has(tri, tuple<int, int, int>{k, i, j})) continue;
-
-                black++;
-                // tri.push_back({i, j, k});
-
-                tri.push_back({i, j, k});
-                tri.push_back({i, k, j});
-                tri.push_back({j, k, i});
-                tri.push_back({j, i, k});
-                tri.push_back({k, j, i});
-                tri.push_back({k, i, j});
-            }
-        }
-    }
-
-    cout << red + black << "\n";
     return 0;
 }
