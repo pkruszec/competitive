4933c8bd00da39926b70ba9743f914c3b415d7cb
Paweł Kruszec <pakruszec@gmail.com>
2025-02-27 12:24:44 +0100

    zaj 100 FL

--- szkopul/23/zaj/zaj.cpp
+++ szkopul/23/zaj/zaj.cpp
@@ -8,8 +8,9 @@ int n, m;
 vector<int> N, M;
 vector<vector<int>> Z;
 vector<int> pa, pb;
+vector<int> mem;
 
-int nwz()
+int nwz1()
 {
     for (int i = 1; i <= n; ++i) {
         Z[i][0] = 0;
@@ -25,6 +26,25 @@ int nwz()
     return Z[n][m];
 }
 
+int nwz()
+{
+    for (int i = 1; i <= n; ++i) {
+        Z[i%2][0] = 0;
+        for (int j = 1; j <= m; ++j) {
+            if (N[i] == M[j] && pa[i] > 0 && pb[j] > 0) {
+                Z[i%2][j] = mem[pb[j]] + 2;
+            } else {
+                Z[i%2][j] = 0;
+            }
+            Z[i%2][j] = max(Z[i%2][j], max(Z[(i-1)%2][j], Z[i%2][j-1]));
+        }
+        for (int j = 1; j <= m; ++j) {
+            if (N[i]==M[j]) mem[j] = Z[(i-1)%2][j-1];
+        }
+    }
+    return Z[n%2][m];
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -34,9 +54,11 @@ int main()
     cin >> n >> m;
     N.resize(n+1);
     M.resize(m+1);
-    Z.resize(n+1, vector<int>(m+1, 0));
+    // Z.resize(n+1, vector<int>(m+1, 0));
+    Z.resize(2, vector<int>(m+1, 0));
     pa.resize(n+1, 0);
     pb.resize(m+1, 0);
+    mem.resize(m+1, 0);
 
     for (int i = 1; i <= n; ++i) cin >> N[i];
     for (int i = 1; i <= m; ++i) cin >> M[i];
